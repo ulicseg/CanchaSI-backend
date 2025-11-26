482c35adb65aaa7b98df6efab0814b8b61bfedf0
ulicseg <ulicseg@gmail.com>
2025-11-26 12:13:46 -0300

    feat: conectar rutas de matchmaking en la app social

--- functions/src/social/index.js
+++ functions/src/social/index.js
@@ -1,20 +1,14 @@
 import express from 'express';
 import cors from 'cors';
-
-// Importamos tus rutas
 import playerRoutes from './routes/player.routes.js';
-
-// import matchmakingRoutes from './routes/matchmaking.routes.js'; // Descomentar luego
+import matchmakingRoutes from './routes/matchmaking.routes.js';
 
 const app = express();
 
-// Configuración básica
 app.use(cors({ origin: true }));
 app.use(express.json());
 
-// Definimos tus rutas base
 app.use('/players', playerRoutes);
-
-// Futuro: app.use('/matchmaking', matchmakingRoutes);
+app.use('/matchmaking', matchmakingRoutes);
 
 export default app;
